fb4bf50e6dcea099be3049e28e60b4171ee14f5d
游子謙 <yochien@youziqiande-MacBook-Pro.local>
2019-06-18 20:35:39 +0800

    change the time to %Y-%m-%d %H:%M

--- websocket.py
+++ websocket.py
@@ -25,7 +25,7 @@ def message_received(client, server, message_data):
         message_data["access_token"]
         message_data["chatroom_secret"]
         message_data["message"]
-        now = datetime.datetime.now()
+        now = datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
         user_name = model.insert_message_to_chatroom_db(message_data["access_token"], message_data["chatroom_secret"], message_data["message"], now)
         if user_name != None:
             print("Client(%d) said: %s" % (client['id'], message_data))
